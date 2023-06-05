ccf16dcffae081582428e1f0c49b78b4c98a9520
Sakhr66 <129528746+Sakhr66@users.noreply.github.com>
2023-06-05 21:58:25 +0300

    Update cart.php
    cart functionality, php

--- cart.php
+++ cart.php
@@ -1,5 +1,4 @@
 use App\Http\Controllers\CartController;
-
 Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
 Route::post('/cart/add/{product}', [CartController::class, 'add'])->name('cart.add');
 Route::patch('/cart/update/{product}', [CartController::class, 'update'])->name('cart.update');
